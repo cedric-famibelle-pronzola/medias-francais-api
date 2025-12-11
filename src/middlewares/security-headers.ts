309698976dbbd4b5de9964e1f29214e087ede1c1
Cédric FAMIBELLE-PRONZOLA <c.pronzola@tuta.io>
2025-12-11 12:24:54 +0400

    fix: add cdn.jsdelivr.net to CSP

--- src/middlewares/security-headers.ts
+++ src/middlewares/security-headers.ts
@@ -15,8 +15,8 @@ export function securityHeaders() {
       ? "default-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' data: https:; connect-src 'self'"
       : [
         "default-src 'self'",
-        "script-src 'self' 'unsafe-inline'",
-        "style-src 'self' 'unsafe-inline'",
+        "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net",
+        "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net",
         "img-src 'self' data: https:",
         "font-src 'self' data:",
         "connect-src 'self'",
